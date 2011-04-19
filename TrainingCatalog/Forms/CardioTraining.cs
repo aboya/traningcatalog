f4c2d7a2161191f5fd6d3c1d0c48825526ade18f
aboimov <aboimov@542dbbd6-26ea-6d31-94aa-2ac446d91fe1>
2011-04-19 12:53:19 +0000

    remove devexpress

--- TrainingCatalog/Forms/CardioTraining.cs
+++ TrainingCatalog/Forms/CardioTraining.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using DevExpress.XtraScheduler;
 
 namespace TrainingCatalog.Forms
 {
@@ -17,18 +16,5 @@ namespace TrainingCatalog.Forms
             InitializeComponent();
         }
 
-        private void schedulerControl1_EditAppointmentFormShowing(object sender, DevExpress.XtraScheduler.AppointmentFormEventArgs e)
-        {
-            Appointment apt = e.Appointment;
-
-            bool openRecurrenceForm = apt.IsRecurring && schedulerStorage1.Appointments.IsNewAppointment(apt);
-
-            using (CardioSession cs = new CardioSession((SchedulerControl)sender, apt, openRecurrenceForm))
-            {
-                e.DialogResult = cs.ShowDialog(this);
-                schedulerControl1.Refresh();
-                e.Handled = true;
-            }
-        }
     }
 }
