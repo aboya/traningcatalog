375653f3f6652d542c9fa41d85429829d9a73efa
aboimov <aboimov@542dbbd6-26ea-6d31-94aa-2ac446d91fe1>
2012-02-21 16:53:55 +0000

    http://code.google.com/p/traningcatalog/issues/detail?id=12

--- TrainingCatalog/Forms/Report.cs
+++ TrainingCatalog/Forms/Report.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Configuration;
 using TrainingCatalog.Forms;
 using TrainingCatalog.BusinessLogic;
+using TrainingCatalog.BusinessLogic.Types;
 
 
 namespace TrainingCatalog
@@ -14,6 +15,7 @@ namespace TrainingCatalog
     {
        
         private DateTime _minDateTime;
+        private BaseReportDay.ProcessIndicator report_ProcessIndicatorEvent;
         protected DateTime MinDateTime
         {
             get
@@ -53,13 +55,19 @@ namespace TrainingCatalog
         {
             InitializeComponent();
         }
-
+ 
         private void Report_Load(object sender, EventArgs e)
         {
             dtpStart.Value = MinDateTime;
             dtpEnd.Value = MaxDateTime;
+            report_ProcessIndicatorEvent = new BaseReportDay.ProcessIndicator(ProcessShow);
+            pbProgress.Maximum = 100;
+            pbProgress.Value = 0;
         }
-
+        private void ProcessShow(int current)
+        {
+            pbProgress.Value = current;
+        }
         private void dtpFrom_ValueChanged(object sender, EventArgs e)
         {
             if (dtpStart.Value < MinDateTime) dtpStart.Value = MinDateTime;
@@ -89,7 +97,15 @@ namespace TrainingCatalog
                     if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                     {
                         string path = saveFileDialog.FileName;
-                        GenerateReport(path, start, end);
+                        BaseReportDay report = null;
+                        if (rbCsv.Checked) report = new ReportCsv(path, start, end) ;
+                        else if (rbHtml.Checked)report =  new ReportHtml(path, start, end) ;
+                        if (report != null)
+                        {
+                            report.ProcessIndicatorEvent += new BaseReportDay.ProcessIndicator(report_ProcessIndicatorEvent);
+                            report.GenerateReport();
+                        }
+
                         MessageBox.Show("Отчет Создан");
                     }
                 }
@@ -103,78 +119,69 @@ namespace TrainingCatalog
             }
             
         }
-        private void GenerateReport(string filePath, DateTime start, DateTime end)
-        {
-            using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.Default))
-            {
-                using (SqlCeConnection connection = new SqlCeConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString))
-                {
-                    connection.Open();
-                    using (SqlCeCommand command = new SqlCeCommand())
-                    {
-                        command.Connection = connection;
-                        command.Parameters.Add("@start", System.Data.SqlDbType.DateTime).Value = start.Date;
-                        command.Parameters.Add("@end", System.Data.SqlDbType.DateTime).Value = end.Date;
-                        command.CommandText = @"select count(Day) from Training " +
-                                                "where Day between @start and  @end ";
+        //private void GenerateReport(string filePath, DateTime start, DateTime end)
+        //{
+        //    using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.Default))
+        //    {
+        //        using (SqlCeConnection connection = new SqlCeConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString))
+        //        {
+        //            connection.Open();
+        //            using (SqlCeCommand command = new SqlCeCommand())
+        //            {
+        //                command.Connection = connection;
+        //                command.Parameters.Add("@start", System.Data.SqlDbType.DateTime).Value = start.Date;
+        //                command.Parameters.Add("@end", System.Data.SqlDbType.DateTime).Value = end.Date;
+        //                command.CommandText = @"select count(Day) from Training " +
+        //                                        "where Day between @start and  @end ";
 
-                        pbProgress.Maximum = Convert.ToInt32(command.ExecuteScalar());
-                        pbProgress.Minimum = 0;
-                        command.CommandText =
-                                      "select Day,Weight,Count,BodyWeight,Exersize.ShortName, Exersize.ID as ExersizeID from (( Link " +
-                                      "inner join Training on Training.ID = Link.TrainingID) " +
-                                      "inner join Exersize on Exersize.ID = Link.ExersizeID ) " +
-                                      "where  " +
-                                      "Day between @start and  @end " +
-                                      "order by Day asc";
+        //                pbProgress.Maximum = Convert.ToInt32(command.ExecuteScalar());
+        //                pbProgress.Minimum = 0;
+        //                command.CommandText =
+        //                              "select Day,Weight,Count,BodyWeight,Exersize.ShortName, Exersize.ID as ExersizeID from (( Link " +
+        //                              "inner join Training on Training.ID = Link.TrainingID) " +
+        //                              "inner join Exersize on Exersize.ID = Link.ExersizeID ) " +
+        //                              "where  " +
+        //                              "Day between @start and  @end " +
+        //                              "order by Day asc";
 
-                        using (SqlCeDataReader dr = command.ExecuteReader())
-                        {
-                            DateTime lastDate = DateTime.MinValue;
-                            DateTime currentDate = DateTime.MinValue;
-                            ReportDayType reportDay = null;
-                            double bodyWeight;
-                            while (dr.Read())
-                            {
-                                currentDate = Convert.ToDateTime(dr["Day"]);
-                                if (dr["BodyWeight"] is DBNull) bodyWeight = 0;
-                                else bodyWeight = Convert.ToDouble(dr["BodyWeight"]);
-                                if (currentDate != lastDate)
-                                {
-                                    lastDate = currentDate;
-                                    if (reportDay != null)
-                                    {
-                                         sw.WriteLine(reportDay.ToString());
-                                         pbProgress.Value++;
-                                         System.Windows.Forms.Application.DoEvents();
-                                    }
-                                    reportDay = new ReportDayType(currentDate, bodyWeight);
-                                }
-                                ReportExersizeType exersize = new ReportExersizeType(Convert.ToInt32(dr["ExersizeID"]),Convert.ToString(dr["ShortName"]), Convert.ToInt32(dr["Count"]), Convert.ToInt32(dr["Weight"]));
-                                reportDay.Add(exersize);
-                            }
-                            if (reportDay != null)
-                            {
-                                sw.WriteLine(reportDay.ToString());
-                            }
+        //                using (SqlCeDataReader dr = command.ExecuteReader())
+        //                {
+        //                    DateTime lastDate = DateTime.MinValue;
+        //                    DateTime currentDate = DateTime.MinValue;
+        //                    ReportDayType reportDay = null;
+        //                    double bodyWeight;
+        //                    while (dr.Read())
+        //                    {
+        //                        currentDate = Convert.ToDateTime(dr["Day"]);
+        //                        if (dr["BodyWeight"] is DBNull) bodyWeight = 0;
+        //                        else bodyWeight = Convert.ToDouble(dr["BodyWeight"]);
+        //                        if (currentDate != lastDate)
+        //                        {
+        //                            lastDate = currentDate;
+        //                            if (reportDay != null)
+        //                            {
+        //                                 sw.WriteLine(reportDay.ToString());
+        //                                 pbProgress.Value++;
+        //                                 System.Windows.Forms.Application.DoEvents();
+        //                            }
+        //                            reportDay = new ReportDayType(currentDate, bodyWeight);
+        //                        }
+        //                        ReportExersizeType exersize = new ReportExersizeType(Convert.ToInt32(dr["ExersizeID"]),Convert.ToString(dr["ShortName"]), Convert.ToInt32(dr["Count"]), Convert.ToInt32(dr["Weight"]));
+        //                        reportDay.Add(exersize);
+        //                    }
+        //                    if (reportDay != null)
+        //                    {
+        //                        sw.WriteLine(reportDay.ToString());
+        //                    }
 
-                        }
-                    }
-                }
-            }
-        }
-        private void WriteDayToStream(StreamWriter sw, ReportDayType day)
-        {
-            //Microsoft.Office.Interop.Excel.Application xla = new Microsoft.Office.Interop.Excel.Application();
-
-            //xla.Visible = true;
-            //Workbook wb = xla.Workbooks.Add(XlSheetType.xlWorksheet);
-            //Worksheet ws = (Worksheet)xla.ActiveSheet;
-            //ws.Cells[0, 2] = "234";
-            
-
-        }
+        //                }
+        //            }
+        //        }
+        //    }
+       // }
 
 
+
+ 
     }
 }
