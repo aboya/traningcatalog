fe5b162425e08f0cdddd4690fbc153e30fa23d1c
aboimov <aboimov@542dbbd6-26ea-6d31-94aa-2ac446d91fe1>
2012-03-23 21:05:33 +0000

    add cardio filtration

--- TrainingCatalog/Forms/CardioResults/CardioResults.cs
+++ TrainingCatalog/Forms/CardioResults/CardioResults.cs
@@ -17,6 +17,8 @@ namespace TrainingCatalog.Forms
     public partial class CardioResults : BaseForm
     {
         Dictionary<int, List<CardioIntervalType>> cardioTrainingReport = new Dictionary<int, List<CardioIntervalType>>();
+        Dictionary<int, string> exersizeNames = new Dictionary<int, string>();
+        CardioFiltrationType filter = null;
         public CardioResults()
         {
             InitializeComponent();
@@ -24,13 +26,17 @@ namespace TrainingCatalog.Forms
 
         private void btnOptions_Click(object sender, EventArgs e)
         {
-            new CardioResultsOptions(cardioTrainingReport).ShowDialog();
+            if(filter == null) {
+                filter = new CardioFiltrationType();
+            }
+            new CardioResultsOptions(cardioTrainingReport, filter).ShowDialog();
             CreateCgraph();
         }
 
         private void CardioResults_Load(object sender, EventArgs e)
         {
             cbType.SelectedIndex = 0;
+            filter = null;
          //   TrainingBusiness.GetCardioResults();
             try
             {
@@ -38,6 +44,7 @@ namespace TrainingCatalog.Forms
                 {
                     using (SqlCeCommand cmd = connection.CreateCommand())
                     {
+
                         connection.Open();
                         var exersizes = TrainingBusiness.GetCardioExersizes(cmd);
                         exersizes.Insert(0,new CardioExersizeType()
@@ -48,7 +55,10 @@ namespace TrainingCatalog.Forms
                         TrainingList.DataSource = exersizes;
                         TrainingList.ValueMember = "Id";
                         TrainingList.DisplayMember = "Name";
-
+                        foreach (var ex in exersizes)
+                        {
+                            exersizeNames[ex.Id] = ex.Name;
+                        }
                         dtpFrom.Value = TrainingBusiness.GetStartTrainingDay(cmd);
                         dtpTo.Value = TrainingBusiness.GetEndTrainingDay(cmd);
                         cardioTrainingReport = TrainingBusiness.GetCardioReport(cmd, dtpFrom.Value, dtpTo.Value);
@@ -120,6 +130,7 @@ namespace TrainingCatalog.Forms
 
                 foreach (CardioIntervalType i in intervals)
                 {
+                    if (Filtration(i)) continue;
                     double value;
                     if (Type == 1) value = i.Velocity;
                     else if (Type == 2) value = i.Time;
@@ -128,8 +139,12 @@ namespace TrainingCatalog.Forms
                     if (i.Date < dtpFrom.Value || i.Date.Date > dtpTo.Value) continue;
                     if (value > 0 && i.Time > 0)
                     {
-                        string tag = string.Format("S/V/T:{0:0.##} | {1} ({2})", value, i.Date.AddSeconds(TotalTime).ToString("dd.MM.yy"), i.Date.AddSeconds(TotalTime).DayOfWeek.ToString());
-                       // string tag = string.Format("{0:0}", value);
+                        string tag = string.Format("S:{0:0.##} V:{1:0.##} T:{2:0.##} | {3} ({4})", 
+                                            i.Distance, i.Velocity, i.Time, 
+                                            i.Date.AddSeconds(TotalTime).ToString("dd.MM.yy"), 
+                                            i.Date.AddSeconds(TotalTime).DayOfWeek.ToString());
+                        if (cardioExersizeId < 0) tag += string.Format("|{0}", exersizeNames[i.CardioTypeId]);
+                        // string tag = string.Format("{0:0}", value);
                         mainIntervals.Add(i.Date.AddSeconds(TotalTime).ToOADate(), 0, tag);
                         mainIntervals.Add(i.Date.AddSeconds(TotalTime).ToOADate(), value, tag);
                         mainIntervals.Add(i.Date.AddSeconds(TotalTime + i.Time).ToOADate(), value, tag);
@@ -146,6 +161,41 @@ namespace TrainingCatalog.Forms
 
             mainGraphControl.AxisChange();
             mainGraphControl.Refresh();
+        }
+        private bool Filtration(CardioIntervalType i)
+        {
+            if (filter != null)
+            {
+                if(filter.DistanceFrom > 0.01) 
+                {
+                    if (i.Distance < filter.DistanceFrom) return true;
+                }
+                if (filter.DistanceTo > 0.01)
+                {
+                    if (i.Distance > filter.DistanceTo) return true;
+                }
+
+                if (filter.VelocityFrom > 0.01)
+                {
+                    if (i.Velocity < filter.VelocityFrom) return true;
+                }
+                if (filter.VelocityTo > 0.01)
+                {
+                    if (i.Velocity > filter.VelocityTo) return true;
+                }
+
+                if (filter.TimeFrom > 0.01)
+                {
+                    if (i.Time < filter.TimeFrom) return true;
+                }
+                if (filter.TimeTo > 0.01)
+                {
+                    if (i.Time > filter.TimeTo) return true;
+                }
+
+            }
+            return false;
+ 
         }
         private void TrainingList_SelectionChangeCommitted(object sender, EventArgs e)
         {
@@ -166,6 +216,11 @@ namespace TrainingCatalog.Forms
         private void dtpTo_ValueChanged(object sender, EventArgs e)
         {
             CreateCgraph();
+        }
+
+        private void baseButton1_Click(object sender, EventArgs e)
+        {
+            CardioResults_Load(null, null);
         }
     }
 }
--- TrainingCatalog/Forms/CardioResults/CardioResultsOptions.Designer.cs
+++ TrainingCatalog/Forms/CardioResults/CardioResultsOptions.Designer.cs
@@ -37,7 +37,21 @@
             this.lblTime = new System.Windows.Forms.Label();
             this.cbTime = new TrainingCatalog.Controls.BaseComboBox();
             this.cbDistance = new TrainingCatalog.Controls.BaseComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDistanceTo = new TrainingCatalog.Controls.FloatNumberTextBox();
+            this.txtDistanceFrom = new TrainingCatalog.Controls.FloatNumberTextBox();
+            this.txtTimeTo = new TrainingCatalog.Controls.FloatNumberTextBox();
+            this.txtTimeFrom = new TrainingCatalog.Controls.FloatNumberTextBox();
+            this.txtSpeedTo = new TrainingCatalog.Controls.FloatNumberTextBox();
+            this.txtSpeedFrom = new TrainingCatalog.Controls.FloatNumberTextBox();
+            this.baseGroupBox1 = new TrainingCatalog.Controls.BaseGroupBox();
+            this.btnApply = new TrainingCatalog.Controls.BaseButton();
             this.gpSettings.SuspendLayout();
+            this.baseGroupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gpSettings
@@ -51,18 +65,18 @@
             this.gpSettings.Controls.Add(this.lblTime);
             this.gpSettings.Controls.Add(this.cbTime);
             this.gpSettings.Controls.Add(this.cbDistance);
-            this.gpSettings.Location = new System.Drawing.Point(12, 12);
+            this.gpSettings.Location = new System.Drawing.Point(15, 7);
             this.gpSettings.Name = "gpSettings";
-            this.gpSettings.Size = new System.Drawing.Size(269, 165);
+            this.gpSettings.Size = new System.Drawing.Size(237, 116);
             this.gpSettings.TabIndex = 15;
             this.gpSettings.TabStop = false;
             this.gpSettings.Text = "Настройки меры";
             // 
             // lblVelocity
             // 
-            this.lblVelocity.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblVelocity.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblVelocity.AutoSize = true;
-            this.lblVelocity.Location = new System.Drawing.Point(18, 27);
+            this.lblVelocity.Location = new System.Drawing.Point(25, 23);
             this.lblVelocity.Name = "lblVelocity";
             this.lblVelocity.Size = new System.Drawing.Size(58, 13);
             this.lblVelocity.TabIndex = 17;
@@ -70,9 +84,9 @@
             // 
             // lblSlash
             // 
-            this.lblSlash.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblSlash.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblSlash.AutoSize = true;
-            this.lblSlash.Location = new System.Drawing.Point(140, 27);
+            this.lblSlash.Location = new System.Drawing.Point(147, 23);
             this.lblSlash.Name = "lblSlash";
             this.lblSlash.Size = new System.Drawing.Size(12, 13);
             this.lblSlash.TabIndex = 20;
@@ -80,11 +94,11 @@
             // 
             // cbSpeedDistance
             // 
-            this.cbSpeedDistance.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cbSpeedDistance.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cbSpeedDistance.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbSpeedDistance.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbSpeedDistance.FormattingEnabled = true;
-            this.cbSpeedDistance.Location = new System.Drawing.Point(82, 23);
+            this.cbSpeedDistance.Location = new System.Drawing.Point(89, 19);
             this.cbSpeedDistance.Name = "cbSpeedDistance";
             this.cbSpeedDistance.Size = new System.Drawing.Size(52, 21);
             this.cbSpeedDistance.TabIndex = 13;
@@ -92,9 +106,9 @@
             // 
             // lblDistance
             // 
-            this.lblDistance.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblDistance.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblDistance.AutoSize = true;
-            this.lblDistance.Location = new System.Drawing.Point(6, 80);
+            this.lblDistance.Location = new System.Drawing.Point(13, 76);
             this.lblDistance.Name = "lblDistance";
             this.lblDistance.Size = new System.Drawing.Size(70, 13);
             this.lblDistance.TabIndex = 19;
@@ -102,11 +116,11 @@
             // 
             // cbSpeedTime
             // 
-            this.cbSpeedTime.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cbSpeedTime.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cbSpeedTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbSpeedTime.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbSpeedTime.FormattingEnabled = true;
-            this.cbSpeedTime.Location = new System.Drawing.Point(158, 23);
+            this.cbSpeedTime.Location = new System.Drawing.Point(165, 19);
             this.cbSpeedTime.Name = "cbSpeedTime";
             this.cbSpeedTime.Size = new System.Drawing.Size(52, 21);
             this.cbSpeedTime.TabIndex = 14;
@@ -114,9 +128,9 @@
             // 
             // lblTime
             // 
-            this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblTime.AutoSize = true;
-            this.lblTime.Location = new System.Drawing.Point(33, 53);
+            this.lblTime.Location = new System.Drawing.Point(40, 49);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(43, 13);
             this.lblTime.TabIndex = 18;
@@ -124,11 +138,11 @@
             // 
             // cbTime
             // 
-            this.cbTime.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cbTime.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cbTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbTime.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbTime.FormattingEnabled = true;
-            this.cbTime.Location = new System.Drawing.Point(82, 50);
+            this.cbTime.Location = new System.Drawing.Point(89, 46);
             this.cbTime.Name = "cbTime";
             this.cbTime.Size = new System.Drawing.Size(52, 21);
             this.cbTime.TabIndex = 15;
@@ -136,21 +150,151 @@
             // 
             // cbDistance
             // 
-            this.cbDistance.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cbDistance.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cbDistance.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbDistance.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbDistance.FormattingEnabled = true;
-            this.cbDistance.Location = new System.Drawing.Point(82, 77);
+            this.cbDistance.Location = new System.Drawing.Point(89, 73);
             this.cbDistance.Name = "cbDistance";
             this.cbDistance.Size = new System.Drawing.Size(52, 21);
             this.cbDistance.TabIndex = 16;
             this.cbDistance.SelectionChangeCommitted += new System.EventHandler(this.cbDistance_SelectionChangeCommitted);
             // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 42;
+            this.label5.Text = "До";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "От";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Расстояние:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "Время:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Скорость:";
+            // 
+            // txtDistanceTo
+            // 
+            this.txtDistanceTo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDistanceTo.Location = new System.Drawing.Point(116, 83);
+            this.txtDistanceTo.Name = "txtDistanceTo";
+            this.txtDistanceTo.Size = new System.Drawing.Size(34, 20);
+            this.txtDistanceTo.TabIndex = 37;
+            // 
+            // txtDistanceFrom
+            // 
+            this.txtDistanceFrom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDistanceFrom.Location = new System.Drawing.Point(76, 83);
+            this.txtDistanceFrom.Name = "txtDistanceFrom";
+            this.txtDistanceFrom.Size = new System.Drawing.Size(34, 20);
+            this.txtDistanceFrom.TabIndex = 36;
+            // 
+            // txtTimeTo
+            // 
+            this.txtTimeTo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTimeTo.Location = new System.Drawing.Point(116, 57);
+            this.txtTimeTo.Name = "txtTimeTo";
+            this.txtTimeTo.Size = new System.Drawing.Size(34, 20);
+            this.txtTimeTo.TabIndex = 35;
+            // 
+            // txtTimeFrom
+            // 
+            this.txtTimeFrom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTimeFrom.Location = new System.Drawing.Point(76, 57);
+            this.txtTimeFrom.Name = "txtTimeFrom";
+            this.txtTimeFrom.Size = new System.Drawing.Size(34, 20);
+            this.txtTimeFrom.TabIndex = 34;
+            // 
+            // txtSpeedTo
+            // 
+            this.txtSpeedTo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSpeedTo.Location = new System.Drawing.Point(116, 31);
+            this.txtSpeedTo.Name = "txtSpeedTo";
+            this.txtSpeedTo.Size = new System.Drawing.Size(34, 20);
+            this.txtSpeedTo.TabIndex = 33;
+            // 
+            // txtSpeedFrom
+            // 
+            this.txtSpeedFrom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSpeedFrom.Location = new System.Drawing.Point(76, 31);
+            this.txtSpeedFrom.Name = "txtSpeedFrom";
+            this.txtSpeedFrom.Size = new System.Drawing.Size(34, 20);
+            this.txtSpeedFrom.TabIndex = 32;
+            // 
+            // baseGroupBox1
+            // 
+            this.baseGroupBox1.Controls.Add(this.btnApply);
+            this.baseGroupBox1.Controls.Add(this.label1);
+            this.baseGroupBox1.Controls.Add(this.label5);
+            this.baseGroupBox1.Controls.Add(this.txtSpeedFrom);
+            this.baseGroupBox1.Controls.Add(this.label4);
+            this.baseGroupBox1.Controls.Add(this.txtSpeedTo);
+            this.baseGroupBox1.Controls.Add(this.label3);
+            this.baseGroupBox1.Controls.Add(this.txtTimeFrom);
+            this.baseGroupBox1.Controls.Add(this.label2);
+            this.baseGroupBox1.Controls.Add(this.txtTimeTo);
+            this.baseGroupBox1.Controls.Add(this.txtDistanceFrom);
+            this.baseGroupBox1.Controls.Add(this.txtDistanceTo);
+            this.baseGroupBox1.Location = new System.Drawing.Point(15, 134);
+            this.baseGroupBox1.Name = "baseGroupBox1";
+            this.baseGroupBox1.Size = new System.Drawing.Size(237, 143);
+            this.baseGroupBox1.TabIndex = 43;
+            this.baseGroupBox1.TabStop = false;
+            this.baseGroupBox1.Text = "Фильтрация";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(148, 108);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(83, 29);
+            this.btnApply.TabIndex = 43;
+            this.btnApply.Text = "Применить";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
             // CardioResultsOptions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 193);
+            this.ClientSize = new System.Drawing.Size(267, 295);
+            this.Controls.Add(this.baseGroupBox1);
             this.Controls.Add(this.gpSettings);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.IsShown = true;
@@ -159,6 +303,8 @@
             this.Load += new System.EventHandler(this.CardioResultsOptions_Load);
             this.gpSettings.ResumeLayout(false);
             this.gpSettings.PerformLayout();
+            this.baseGroupBox1.ResumeLayout(false);
+            this.baseGroupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -174,6 +320,19 @@
         private System.Windows.Forms.Label lblTime;
         private Controls.BaseComboBox cbTime;
         private Controls.BaseComboBox cbDistance;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Controls.FloatNumberTextBox txtDistanceTo;
+        private Controls.FloatNumberTextBox txtDistanceFrom;
+        private Controls.FloatNumberTextBox txtTimeTo;
+        private Controls.FloatNumberTextBox txtTimeFrom;
+        private Controls.FloatNumberTextBox txtSpeedTo;
+        private Controls.FloatNumberTextBox txtSpeedFrom;
+        private Controls.BaseGroupBox baseGroupBox1;
+        private Controls.BaseButton btnApply;
 
     }
 }
